1c36633c3cf0d2bfd1461e42289d72651d39aaeb
Jonathan Hollenbeck <jonikeule@web.de>
2018-12-17 15:16:10 +0100

    checkFastaFormat neu geshrieben, mit True und False und Kommentar ignor

--- Program_neu.py
+++ Program_neu.py
@@ -1,5 +1,7 @@
 import argparse
 
+#declaring Methods
+
 #read in all Arguments
 def readInArguments():
     parser = argparse.ArgumentParser()
@@ -22,7 +24,7 @@ def openFileStream(file):
     try:
         return open(file, "r")
     except IOError:
-        print("An error occured trying to read " + file)
+        print("ERROR: An error occured trying to read " + file)
         exit(1)
 
 #parsing the content of a file into a list
@@ -33,12 +35,45 @@ def parseFileToList(stream):
         fastaList.append(line)
     return fastaList
 
+def checkFastaFormat(fileContentAsList, filename):
+    inGeneID = False
+    inSequence = False
+    for line in fileContentAsList:
+        if not line.startswith(";"):
+            if inGeneID == False and line.startswith(">"):
+                inGeneID = True
+                inSequence = False
+            elif inGeneID == True and line.startswith(">"):
+                print("ERROR: " + filename + "is not in FASTA format, because there where 2 GenIDs in a row")
+                exit(1)
+            elif inGeneID == True:
+                if not line.isalpha():
+                    print("ERROR: " + filename + "is not in FASTA format, because of this line: " + line)
+                    exit(1)
+                inGeneID = False
+                inSequence = True
+            elif inSequence == True:
+                if not line.isalpha():
+                    print("ERROR: " + filename + "is not in FASTA format, because of this line: " + line)
+                    exit(1)
+            else:
+                print("ERROR: " + filename + "is not in FASTA format, because of this line: " + line)
+                exit(1)
+
+
+def compareFastas(fasta1ContentAsList, fasta2ContentAsList):
+    print("compareFastas")
+
+#actually doing stuff now
+
 readInArguments();
 
 #reading in the original fasta file
 ogFastaStream = openFileStream("input_files/" + args.originalfasta)
 ogFasta = parseFileToList(ogFastaStream)
 ogFastaStream.close()
+
+checkFastaFormat(ogFasta, "input_files/" + args.originalfasta)
 
 #putting all genes from originalfasta in queries
 if "all" in args.queries:
@@ -61,3 +96,5 @@ for fasta in args.comparefastas:
     cpFastaStream = openFileStream("input_files/" + fasta)
     cpFasta = parseFileToList(cpFastaStream)
     cpFastaStream.close()
+
+    checkFastaFormat(cpFasta, "input_files/" + fasta)
